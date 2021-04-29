49db93d8bb15fb7af348ac3002438e68ae1946ae
Gowtham-Muthyala <73606221+Gowtham-Muthyala@users.noreply.github.com>
2021-04-29 23:33:09 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,8 +24,3 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
-
-# echo '-------------------------------------------------------------------------------'
-# echo 'Building a Docker Image'
-# echo '-------------------------------------------------------------------------------'
-docker build -t gowthammuthyala/java-proj .
